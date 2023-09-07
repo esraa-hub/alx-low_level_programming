cf16c011628181301db315a1967967cbe6e75d4a
esraa_hub <engesraa3@gmail.com>
2023-09-07 14:42:35 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/*Description:aCprogram-using-puts-functionn*/
+/**
+ *main-EntryPoint
+ *
+ *description:aCprogram-using-puts-functionn
+ *
+ *return-always-0-success
+*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
