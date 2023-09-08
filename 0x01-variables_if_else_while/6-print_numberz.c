6416ec3f8abd82df9fd2d6873dc9e68bde529b25
esraa_hub <engesraa3@gmail.com>
2023-09-08 16:23:39 -0700

    v

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A c programm that print with printf furncyion.
+ *
+ * Return: Always 0 (success).
+*/
+
+int main(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
