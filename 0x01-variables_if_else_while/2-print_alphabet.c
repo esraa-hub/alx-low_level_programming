4a08af4808979c774f19d4a1b1caa857599578e5
esraa_hub <engesraa3@gmail.com>
2023-09-08 15:05:04 -0700

    v

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A c programm that print with printf furncyion.
+ *
+ * Return: Always 0 (success).
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i >= 'z' ; ++i)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
