fde003e78a736a17e2535e208d098cbe2e9adb69
esraa_hub <engesraa3@gmail.com>
2023-09-08 17:07:30 -0700

    v

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A c programm that print with printf furncyion.
+ *
+ * Return: Always 0 (success).
+*/
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 102)
+	{
+		putchar(i);
+		if (i > 57)
+			i += 39;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
