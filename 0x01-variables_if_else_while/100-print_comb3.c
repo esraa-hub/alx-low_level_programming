3f5ceae8050a5995ed3bb8b61d4df0e4b6822c35
esraa_hub <engesraa3@gmail.com>
2023-09-09 11:03:24 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@
 
 int main(void)
 {
-	char i = 0, j = 0;
+	int i = 0, j = 0;
 
-	for (i = '0'; i <= '9'; ++i)
+	for (i = 0; i <= 9; ++i)
 	{
-		for (j = '0'; j <= '9'; ++j)
+		for (j = 0; j <= 9; ++j)
 		{
 			if (i < j)
 			{
-				putchar (i);
-				putchar (j);
+				putchar (i + 48);
+				putchar (j + 48);
 				putchar (',');
 				putchar (' ');
 			}
