38abb480f94334041ea95e5087b1b86a3f8f7230
esraa_hub <engesraa3@gmail.com>
2023-09-09 11:56:59 -0700

    m

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A c programm that print with printf furncyion.
+ *
+ * Return: Always 0 (success).
+*/
+
+int main(void)
+{
+	int i = 0, j = 0, h = 0, k = 0;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			for (h = 0; h <= 9; ++h)
+			{
+				for (k = 0; k <= 9; ++k)
+				{
+					putchar (i + 48);
+					putchar (j + 48);
+					putchar (' ');
+					putchar (h + 48);
+					putchar (h + 48);
+					if (i + j + h + k != 25)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
