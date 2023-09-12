6e2a1da68d1451c34877d0827af66f71db419e69
esraa_hub <engesraa3@gmail.com>
2023-09-12 16:51:28 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - prints the alphabet.
+ *
+ * Description: A c programm that print with printf furncyion.
+*/
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar (i);
+	}
+	_putchar ('\n');
+}
