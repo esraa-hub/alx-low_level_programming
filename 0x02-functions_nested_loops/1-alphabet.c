8e48e09dac694be6637245b04314a3fe308738e6
esraa_hub <engesraa3@gmail.com>
2023-09-12 16:41:35 -0700

    l

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - prints the alphabet.
+ *
+ * Description: A c programm that print with printf furncyion.
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z'; i++)
+		_putchar (i);
+	_putchar ('\n');
+}
