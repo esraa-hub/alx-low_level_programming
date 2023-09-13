6cd47d65234c61680df6084276e3e5b531e0ad81
esraa_hub <engesraa3@gmail.com>
2023-09-12 18:32:09 -0700

    l

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - function that checks the number sign
+ *
+ * @n: the number to be checked
+ *
+ * Return: return 1 if 'n' is positive
+ * otherwise  0 if 'n' is zero
+ *  and -1 if 'n' is negative
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('0');
+		return (-1);
+	}
+
+
+}
