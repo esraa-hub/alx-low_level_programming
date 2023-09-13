856ddecfcdaeb9053ee3d9cb1e90f90db6aaea50
esraa_hub <engesraa3@gmail.com>
2023-09-12 20:03:26 -0700

    k

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function that checks the number sign
+ *
+ * @a: the number to be checked
+ * @b: the number to be checked
+ *
+ * Return: return 1 if 'n' is positive
+ * otherwise  0 if 'n' is zero
+ *  and -1 if 'n' is negative
+ *
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
