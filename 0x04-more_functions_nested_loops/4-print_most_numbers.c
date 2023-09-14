14e6795bb49ffd1793b2d80de21c049e63d85a47
esraa_hub <engesraa3@gmail.com>
2023-09-14 15:20:29 -0700

    M

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  print_most_numbers -function that checks the number sign
+ *
+ * Return: return 1 if 'c' is positive
+ * otherwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar (i);
+		if (i == 50 || i == 52)
+			continue;
+	}
+	_putchar ('\n');
+}
