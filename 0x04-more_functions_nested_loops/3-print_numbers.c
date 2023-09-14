16341d455d7d8852f86f6a89eb0ce7455b1dc410
esraa_hub <engesraa3@gmail.com>
2023-09-14 14:57:09 -0700

    M

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_numbers -function that checks the number sign
+ *
+ * Return: return 1 if 'c' is positive
+ * otherwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar (i);
+}
