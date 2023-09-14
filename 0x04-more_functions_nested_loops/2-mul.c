f199b0c45a84321acac03cf8d7eef3818beac1ad
esraa_hub <engesraa3@gmail.com>
2023-09-14 14:51:57 -0700

    M

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  mul -function that checks the number sign
+ *
+ * @a: the number to be checked
+ * @b: the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * otherwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
