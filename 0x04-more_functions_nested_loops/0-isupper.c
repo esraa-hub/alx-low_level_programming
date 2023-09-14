970eafb09cd7953627388fc1c4d2a17ebea82a90
esraa_hub <engesraa3@gmail.com>
2023-09-14 14:05:49 -0700

    A

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  _isupper- function that checks the number sign
+ *
+ * @c: the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * otherwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+	 _putchar ('\n');
+
+}
