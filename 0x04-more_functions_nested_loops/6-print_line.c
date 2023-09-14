bbc55488482506142ea009c409a8b29164580b29
esraa_hub <engesraa3@gmail.com>
2023-09-14 16:24:46 -0700

    M

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,11 +14,12 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
-	{
-		for (i = 1; i >= n ; i++)
-			_putchar ('_');
-	}
-	else
+	if (n <=  0)
 		_putchar ('\n');
+	else
+	{
+		for (i = 1; i <= n ; i++)
+			_putchar ('_');
+		_putchar ('\n');
+	}
 }
