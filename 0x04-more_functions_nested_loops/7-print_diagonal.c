a072bd653f36f9d91c514313913be13e52148173
esraa_hub <engesraa3@gmail.com>
2023-09-14 16:38:36 -0700

    M

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *  print_diagonal -function that checks the number sign
+ *
+ *  @n :lkcmsdlvmkslmslm
+ *
+ * Return: return 1 if 'c' is positive
+ * otherwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <=  0)
+		_putchar ('\n');
+	else
+	{
+		for (i = 1; i <= n ; i++)
+		{
+			for (j = 1; j = i; j++)
+			{
+				_putchar ('');
+			}
+			_putchar ('\');
+			_putchar ('\n');
+		}
+}
