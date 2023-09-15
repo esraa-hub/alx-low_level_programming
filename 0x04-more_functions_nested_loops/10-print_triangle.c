726ceb5f10a70337254a4e70d269a0bbbf997668
esraa_hub <engesraa3@gmail.com>
2023-09-14 18:21:34 -0700

    M

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,24 +10,25 @@
  *  and -1 if 'c' is negative
  *
  */
+
 void print_triangle(int size)
 {
 	int i, j;
 
 	if (size <= 0)
-		_putchar ('\n');
+		_putchar('\n');
 	else
 	{
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size; j++)
 			{
-				if ((i * j) <= size)
-					_putchar (" ");
+				if ((i + j) <= size)
+					_putchar(' ');
 				else
-					_putchar ("#");
+					_putchar('#');
 			}
-			_putchar ("\n");
+			_putchar('\n');
 		}
 	}
 }
