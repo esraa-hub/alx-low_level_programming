5e51caaab770f3efb6ead417a6ead31398b5a2cf
esraa_hub <engesraa3@gmail.com>
2023-09-14 17:06:50 -0700

    M

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  print_square -function that checks the number sign
+ *
+ * @size :vjnskdcvnksj
+ *
+ * Return: return 1 if 'c' is positive
+ * otherwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar ('\n');
+	else
+	{
+		for (i = 0; i <= size ; i++)
+		{
+			for (j = 0; j <= size ; j++)
+				_putchar (35);
+			-putchar ('\n');
+		}
+	}
+
+}
