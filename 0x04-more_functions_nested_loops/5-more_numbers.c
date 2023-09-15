1271b06233120ce14c98b0412278a1c2799083fa
esraa_hub <engesraa3@gmail.com>
2023-09-14 18:39:08 -0700

    M

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,17 @@
  */
 void more_numbers(void)
 {
-	int i, sec;
+	int i, j, sec;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10 ; j++)
 	{
-		if (i > 9)
-			_putchar(1 + 48);
-		sec = i % 10;
-		_putchar(sec + 48);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(1 + 48);
+			sec = i % 10;
+			_putchar(sec + 48);
+		}
+		_putchar('\n'); 
 	}
-	_putchar('\n');
 }
