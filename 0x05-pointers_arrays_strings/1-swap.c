b68f3ddbb24d5725aa49a605d4c8e45ba4efa5b9
esraa-hub <engesraa3@gmail.com>
2023-09-18 17:50:56 -0700

    n

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  swap_int - function that checks the number sign
+ *
+ * @a : the number to be checked
+ * @b : the number to be swap
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *emp;
+	emp = a;
+	a = b;
+	b = emp;
+}
