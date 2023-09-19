35e0bc79498a9a95c71c6d72c3d7d6ca5eac40fb
esraa-hub <engesraa3@gmail.com>
2023-09-19 04:50:23 -0700

    m

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  *_strcpy -function that checks the number sign
+ *
+ *  @src : array iks kcmsdlvmkslmslm
+ *  @dest : size of array
+ *
+  * Return: return 1 if 'c' is positive
+ * otherwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] = '\0'; ++i)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
