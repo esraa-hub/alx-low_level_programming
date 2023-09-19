e472e707e82b9ed68f008b44c777067751186ae2
esraa-hub <engesraa3@gmail.com>
2023-09-19 04:06:08 -0700

    m

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  print_array -function that checks the number sign
+ *
+ *  @a : array ls iks kcmsdlvmkslmslm
+ *  @n : size of array
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf('%d, ', a[i]);
+		printf('%d', a[i]);
+	}
+}
