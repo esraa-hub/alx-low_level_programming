3de4275e61cfe49a9b95471f4d7a510255fd27d9
esraa-hub <engesraa3@gmail.com>
2023-09-21 14:47:22 -0700

    v

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  _strncpy -function that checks the number sign
+ *
+ * @dest: the number to be checked
+ * @src: the number to be checked
+ * @n : the numbber of b=pru=onter
+ * Return: return 1 if 'c' is positive
+ * ot00herwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
