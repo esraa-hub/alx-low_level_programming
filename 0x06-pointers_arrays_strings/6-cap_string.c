077278c48d2ba874bc79c5cc92251db278f2d581
esraa-hub <engesraa3@gmail.com>
2023-09-21 18:38:38 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,67 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * isLower-function that checks the number sign
- *
- * @c : the number to be checked
- * Return: return 1 if 'c' is positive
- * ot00herwise  0 if 'c' is zero
- *  and ss1 if 'c' is negative
- *
- */
-int isLower(char c)
-{
-	return (c >= 97 && c <= 122);
-}
-/**
- * isDelimiter -function that checks the number sign
- *
- * @c : the number to be checked
- * Return: return 1 if 'c' is positive
- *  and ss1 if 'c' is negative
- *
- */
-int isDelimiter(char c)
-{
-	int i;
-	char  delimiter[] = " \t\n,.!?\"(){}";
-
-	for (i = 0; i < 12; i++)
-	{
-		if (c == delimiter[i])
-			return  (1);
-		return (0);
-	}
-}
 /**
  * cap_string -function that checks the number sign
  *
- * @s : the number to be checked
+ * @str : the number to be checked
  * Return: return 1 if 'c' is positive
  * ot00herwise  0 if 'c' is zero
  *  and ss1 if 'c' is negative
- *
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	char *ptr = s;
-	int findDelimter = 0;
+	int i = 0;
 
-	while (*s)
+	while (str[i])
 	{
-		if (isDelimiter(*s))
-			findDelimter = 1;
-		else if (isLower(*s) && findDelimter == 0)
+		while (!(str[i] > 'a' && str[i] < 'z'))
 		{
-			*s -= 32;
-			findDelimter = 0;
+			i++;
+			if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '\"' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			str[i - 1] == ';' ||
+			i == 0)
+				str[i] -= 32;
 		}
-		else
-		{
-			findDelimter = 0;
-		}
-		s++;
+		i++;
 	}
-	return (ptr);
+	return (str);
 }
