4678fd5c5db9a9a4d0194a644069bd04a070e8ad
esraa-hub <engesraa3@gmail.com>
2023-09-21 18:44:50 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,6 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		{
 			i++;
 			if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
@@ -31,7 +30,6 @@ char *cap_string(char *str)
 			str[i - 1] == ';' ||
 			i == 0)
 				str[i] -= 32;
-		}
 		i++;
 	}
 	return (str);
