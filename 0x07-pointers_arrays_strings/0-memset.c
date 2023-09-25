1849fde3264ed774c3c9535d41edf0e9ae3a5f78
esraa-hub <engesraa3@gmail.com>
2023-09-25 16:27:45 -0700

    n

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -16,9 +16,9 @@ char *_memset(char *s, char b, unsigned int n)
 	 unsigned int i;
 
 	i = 0;
-	for (i = 0; n = 0; i++, n--)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
-	}
+		}
 	return (s);
 }
