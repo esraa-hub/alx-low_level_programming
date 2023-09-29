2c8fa55b69b5d1461df118868039ed96d4450ce8
esraa-hub <engesraa3@gmail.com>
2023-09-29 11:42:12 -0700

    m

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion-function that checks the number sign
+ *
+ * @n : the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * ot00herwise i 0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ */
+int _sqrt_recursion(int n);
+{
+	return (square(n, 1));
+}
+/**
+ *  square -function that checks the number sign
+ *
+ * @n : the number to be checked
+ * @val : the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * ot00herwise i 0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ */
+int square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else
+	{
+		if (val * val < n)
+		return (square(n, val + 1));
+	}
+	else
+		return (-1);
+}
