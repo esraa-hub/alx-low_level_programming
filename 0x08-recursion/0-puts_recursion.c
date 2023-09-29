c88a4f0d162fdc197f5484cc5e4c95d6523d5443
esraa-hub <engesraa3@gmail.com>
2023-09-28 17:15:12 -0700

    m

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (!*s)
+	if (*s == '\0')
 	{
-		return;
+		__putchar('\n');
 	}
+	else
+	{
 	_putchar(*s);
-	s++;
-	_puts_recursion(char *s);
+	_puts_recursion(s+1);
+	}
 }
