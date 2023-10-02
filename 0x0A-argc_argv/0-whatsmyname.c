eb631f13c36b9216e3f0295207161da6dc4b29d4
esraa-hub <engesraa3@gmail.com>
2023-10-02 14:12:53 -0700

    k

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 /**
- * main- prints program
- * * @argc :cjnkcjnkjdcnckjn
- * @argv :lcnskcnjskcnskcnks
- * return :0
-*/
+ *  main-function that checks the number sign
+ *
+ * @argv : the number to be checked
+ * @argc : the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * ot00herwise i 0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ */
 int main(int argc, char const *argv[])
 {
 	(void)argc;
