4d54a717c23daadf5e28c89ec6ad3f27c1a8904a
esraa-hub <engesraa3@gmail.com>
2023-10-02 14:00:28 -0700

    k

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- prints program
+ * * @argc :cjnkcjnkjdcnckjn
+ * @argv :lcnskcnjskcnskcnks
+ * return :0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	print ("%s\n", argv[0]);
+	return (0);
+}
