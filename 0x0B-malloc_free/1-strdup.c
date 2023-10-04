9990cb10b4cd63633f687d2feec40e240762cb63
esraa-hub <engesraa3@gmail.com>
2023-10-04 13:23:12 -0700

    m

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  _strdup-function that checks the number sign
+ *
+ * @str: the number if byte
+ * Return: return 1 if 'c' is positive
+ * ot00herwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+char *_strdup(char *str)
+{
+	int size, i;
+	char *m;
+
+	if (*str == 0)
+		return (NULL);
+	for (; str[size]; size++)
+		;
+	m = malloc(size * sizeof(*str) + 1);
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
+
