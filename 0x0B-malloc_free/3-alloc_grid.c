564635fc74576833df14cc2d73f5f8cae511493a
esraa-hub <engesraa3@gmail.com>
2023-10-05 02:10:26 -0700

    m

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *  alloc_grid-function that checks the number sign
+ *
+ * @width: the number if byte
+ * @height:ber if byte
+ * Return: return 1 if 'c' is positive
+ * ot00herwise  0 if 'c' is zero
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int **tab, i, j;
+
+	tab = malloc(sizeof(*tab) * height);
+
+	if (width <= 0 || height <= 0 || tab == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i <= height; i++)
+		{
+			tab[i] = malloc(sizeof(*tab) * width);
+			if (tab[i] == 0)
+			{
+				while (--i)
+				{
+					free(tab[i]);
+				}
+				free(tab);
+				return (NULL);
+			}
+			for (j = 0; j <= width; j++)
+					tab[i][j] = 0;
+		}
+	}
+	return (tab);
+}
