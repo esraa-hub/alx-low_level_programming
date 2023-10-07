6f50ab3fec28afbb924d74d4e1d8cc784b558d09
esraa-hub <engesraa3@gmail.com>
2023-10-07 14:42:57 -0700

    n

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,15 +12,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
+	int i, j, sum = 0;
 	char *c;
 
-	while (--argc)
+	for (int i = 1; i < argc; i++)
 	{
-		for (c = argv[argc]; *c; c++)
-			if (*c < '0' || *c > '9')
-				return (prinf("Error\n"), 1);
-		sum += atoi(argv[argc]);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				prinf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
