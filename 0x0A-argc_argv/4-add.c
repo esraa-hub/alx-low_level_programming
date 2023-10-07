aa33f13de9e8d6fe18d0bd222573dee41ce76b7f
esraa-hub <engesraa3@gmail.com>
2023-10-07 14:03:16 -0700

    n

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main-function that checks the number sign
+ *
+ * @argv : the number to be checked
+ * @argc : the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * ot00hrwise i 0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ */
+int main(int argc, char const *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (prinf("Error\n"), 1);
+			sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
