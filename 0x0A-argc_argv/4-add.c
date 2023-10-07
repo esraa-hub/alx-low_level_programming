8f9974a0ae5d5571c3a27e9bd12e7e776fde406c
esraa-hub <engesraa3@gmail.com>
2023-10-07 14:13:45 -0700

    n

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,13 +18,9 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-		{
 			if (*c < '0' || *c > '9')
-			{
 				return (prinf("Error\n"), 1);
-			}
 			sum += atoi(argv[argc]);
-		}
 	}
 	printf("%d\n", sum);
 	return (0);
