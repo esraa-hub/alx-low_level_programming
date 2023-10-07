5820643d1bd272666211099d3f2ba1d56b051f4d
esraa-hub <engesraa3@gmail.com>
2023-10-07 13:23:55 -0700

    n

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main-function that checks the number sign
+ *
+ * @argv : the number to be checked
+ * @argc : the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * ot00herwise i 0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ */
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+	while (i--)
+	{
+		printf("%s\n", argc[i]);
+		i++;
+	}
+	return (0);
+}
