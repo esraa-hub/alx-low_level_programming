63d5d6f4e3de474c028c9d7a06bb7a1dff160ce3
esraa-hub <engesraa3@gmail.com>
2023-10-09 14:29:14 -0700

    m

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _calloc-function that checks the number sign
+ *
+ * @size: the number to be checked
+ * @nmemb: the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	 void *ptr;
+
+	 if (nmemb == 0 || size == 0)
+		 return (NULL);
+	 ptr = malloc(sizeof(int) * nmemb);
+	 if (ptr == NULL)
+		 return (NULL);
+	 memset(ptr, 0, total_size);
+	 return (ptr);
+}
