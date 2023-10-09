a0784f8b8d01cd190b5e284bf19dabb043be1d0f
esraa-hub <engesraa3@gmail.com>
2023-10-09 02:19:15 -0700

    M

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  malloc_checked-function that checks the number sign
+ *
+ * @b: the number to be checked
+ *
+ * Return: return 1 if 'c' is positive
+ * ot00herwise  0 if 'c' is zero
+ *  and -1 if 'c' is negative
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = (int *)malloc(sizeof(int));
+
+	if (pt == null)
+		exit(98);
+	return (ptr);
+}
