d8bdb6530514cbaea51d107c372733e0eea4dc90
esraa-hub <engesraa3@gmail.com>
2023-10-10 00:54:38 -0700

    m

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- function that checks for lowercase character
+ *
+ * Return: return 1 if 'c' is lowercase
+ * otherwise  always 0(success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
