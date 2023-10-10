4f493b0e5a593cf079d115f4cbb153c1d775eae9
esraa-hub <engesraa3@gmail.com>
2023-10-10 01:36:46 -0700

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- function that checks for lowercase character
+ *
+ * @name: the charcter to be checked
+ * @age: the charcter to be checked
+ * @owner: the charcter to be checked
+ *
+ * Description: return 1 if 'c' is lowercase
+ * otherwise  always 0(success)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
