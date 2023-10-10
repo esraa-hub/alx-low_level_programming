941a323d1e225f92dd7ef8e851d06d2b244211ec
esraa-hub <engesraa3@gmail.com>
2023-10-10 13:25:48 -0700

    l

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,11 +10,12 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
+	if (d)
 	{
-		return;
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
 	}
-	free(d->name);
-	free(d->owner);
-	free(d);
 }
