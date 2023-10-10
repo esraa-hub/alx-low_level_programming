2d999315e322cbe57b2c4f03d91981c87433aa88
esraa-hub <engesraa3@gmail.com>
2023-10-10 12:25:05 -0700

    l

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name ?  d->name : "(nil)");
 		printf("Age: %d\n", d->age);
-		printf("Owner: %s\n", d->Owner ? d->Owner : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
 
