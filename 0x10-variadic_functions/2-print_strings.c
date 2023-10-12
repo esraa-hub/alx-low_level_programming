41c8d7360fd530038918d319b412c93fb27fda69
esraa-hub <engesraa3@gmail.com>
2023-10-12 14:04:58 -0700

    j

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_strings-returns the sum of all its parameters.
+ *
+ * @n:The number of parametrs.
+ * @...:The parameters to be summed.
+ * @separator:theseparator between.
+ *
+ * Return:The sum of all the parameters.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	char *str;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%d%s", (str = va_arg(ap, char *)) ? str : "(nill)",
+				i ? (separator ? separator : " ") : "\n");
+	va_end(ap);
+}
