8fcc0b7f06e5da1570b613ec5bcb505a15d93628
esraa-hub <engesraa3@gmail.com>
2023-10-13 05:15:59 -0700

    g

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "FUNCTION_POINTERS"
+/**
+ * print_name-THE FUNJHSIDFH.
+ *
+ * @name:the name to type.
+ * @f: the pointer to function name.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
