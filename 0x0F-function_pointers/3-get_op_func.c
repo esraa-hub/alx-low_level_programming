80d6864e1a8d8eb2f0a87b0330bd42fd8d746ccb
esraa-hub <engesraa3@gmail.com>
2023-10-13 18:43:20 -0700

    m

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,19 +10,20 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{ "+", add },
-		{ "-", sub },
-		{ "*", mul },
-		{ "/", div },
-		{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
-	while (1 < 5)
+	while (i < 5)
 	{
 		if (s && s[0] == ops[i].op[0] && !s[1])
 		{
-			return (ops[i].func);
+			return (ops[i].f);
 		}
 		i++;
 	}
