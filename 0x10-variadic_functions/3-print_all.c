cd164184e2ee3e0a0e51d801bf5c4c650bd4b09d
esraa-hub <engesraa3@gmail.com>
2023-10-13 17:48:51 -0700

    m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,18 +1,51 @@
 #include "variadic_functions.h"
-/**void formate_char(char *separator, va_list ap)
+/**
+ * format_char-returns the sum of all its parameters.
+ *
+ * @separtor:The number of parametrs.
+ * @ap:The parameters to be summed.
+ */
+void format_char(char *separator, va_list ap)
 {
 	printf("%s%c", separator, va_arg(ap, int));
 }
-void formate_int(char *separator, va_list ap)
+/**
+ * format_int-returns the sum of all its parameters.
+ *
+ * @separtor:The number of parametrs.
+ * @ap:The parameters to be summed.
+ */
+void format_int(char *separator, va_list ap)
 {
 	printf("%s%d", separator, va_arg(ap, int));
-
 }
-void formate_string(char *separator, va_list ap)
+/**
+ * format_float-returns the sum of all its parameters.
+ *
+ * @separtor:The number of parametrs.
+ * @ap:The parameters to be summed.
+ */
+void format_float(char *separator, va_list ap)
 {
 	printf("%s%f", separator, va_arg(ap, double));
 }
-*/
+/**
+ * format_string-returns the sum of all its parameters.
+ *
+ * @separtor:The number of parametrs.
+ * @ap:The parameters to be summed.
+ */
+void format_string(char *separator, va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	switch ((int)(!str))
+	{
+		case 1:
+			str = "(nil)";
+	}
+	printf("%s%s", separator, str);
+}
 /**
  * print_strings-returns the sum of all its parameters.
  *
@@ -24,43 +57,32 @@ void formate_string(char *separator, va_list ap)
  */
 void print_all(const char *const format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	int i = 0, j;
+	char *separator = "";
+	va_list ap;
 
-    int i = 0;
-    char c;
-    float f;
-    char *s;
-
-    while (format[i] != '\0')
-    {
-        if (format[i] == 'c')
-        {
-            c = (char)va_arg(args, int);
-            printf("%c", c);
-        }
-        else if (format[i] == 'i')
-        {
-            int num = va_arg(args, int);
-            printf("%d", num);
-        }
-        else if (format[i] == 'f')
-        {
-            f = (float)va_arg(args, double);
-            printf("%f", f);
-        }
-        else if (format[i] == 's')
-        {
-            s = va_arg(args, char *);
-            if (s == NULL)
-                printf("(nil)");
-            else
-                printf("%s", s);
-        }
-
-        i++;
-    }
-
-    va_end(args);
-    printf("\n");
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (tokens[j].token)
+		{
+			if (format[i] == tokens[j].token[0])
+			{
+				token[j].(separator.ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,17 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * struct token- struct token
+ *
+ * @token:ddjgj.
+ * @f:hfguhgidi.
+ */
+typedef struct token
+{
+	char token;
+	void (*f)(char *, va_list);
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_all(const char * const format, ...);
