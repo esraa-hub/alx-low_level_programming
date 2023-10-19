3d763ac621be708ce0971b7c32621d4daae3b9d9
esraa-hub <engesraa3@gmail.com>
2023-10-19 13:50:39 -0700

    the f

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "list.h"
+/**
+ * print_list-thefunction to print the listed link
+ *
+ * @h:the linked list to be printed
+ *
+ * Return: the size of the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str == Null)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d}%s\n", h->len, h->str);
+			h = h->next;
+			counter++;
+		}
+	}
+	return (counter);
+}
