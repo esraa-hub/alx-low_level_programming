970a1ba3b100690bf9efc42a555c8350d9eedad5
esraa-hub <engesraa3@gmail.com>
2023-10-19 14:07:46 -0700

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,8 +16,8 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d}%s\n", h->len, h->str);
-		h = h->next;
 		counter++;
+		h = h->next;
 	}
 	return (counter);
 }
