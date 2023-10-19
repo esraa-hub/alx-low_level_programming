f830b86e84b6bdc758eb131508faabfc938fb9ff
esraa-hub <engesraa3@gmail.com>
2023-10-19 16:42:10 -0700

    l

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "list.h"
+/**
+ * free_list-the function
+ *
+ * @head:the address
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (tmp != NULL)
+	{
+		temp = temp->next;
+		free(temp->str);
+		free(temp);
+	}
+
+}
