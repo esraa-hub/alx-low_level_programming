e002ddebb93fe48c8d894f8e0b7baf0e6d0582df
esraa-hub <engesraa3@gmail.com>
2023-10-23 03:13:46 -0700

    message

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+/**
+ * listint_len-thefun.
+ *
+ * @h:theinput.
+ *
+ * Return:the output.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
