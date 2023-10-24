e00b8f8ff4f87f372cb962834b2a33374dd760a4
esraa-hub <engesraa3@gmail.com>
2023-10-24 08:02:19 -0700

    G

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,30 @@
 #include "lists.h"
-/***/
+/**
+ * insert_nodeint_at_index- thefun.
+ *
+ * @head:the arg.
+ * @idx:thearg.
+ * @n:thearg
+ * Return:thefri
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int i;
+	listint_t *new, *trans;
 
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (-1);
+	new->n = n;
+	trans = *head;
+	for (i = 0; i < idx; i++)
+	{
+		if (trans == NULL)
+			return (-1);
+		trans = trans->next;
+	}
+	new->next = trans->next;
+	trans->next = new;
+	return (new);
 }
+
