fb26078e53e55601509fa49a03252cdb911c7f40
esraa-hub <engesraa3@gmail.com>
2023-10-24 07:30:42 -0700

    G

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,8 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	int i;
 
 	for (i = 0; i < index; i++)
-		 head = head->next;
-	if (head == NULL)
-		return (NULL);
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
 	return (head);
 }
