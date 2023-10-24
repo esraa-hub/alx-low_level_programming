34991c4af7157b10a35963c96eaa0f613c71adc0
esraa-hub <engesraa3@gmail.com>
2023-10-24 06:41:03 -0700

    G

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,16 +7,12 @@
  */
 int sum_listint(listint_t *head);
 {
-	listint_t *trans;
 	int sum = 0;
 
-	if (!head)
-		return (0);
-	trans = *head;
-	while (!trans->next)
+	while (head)
 	{
-		sum += trans->n;
-		trans = trans->next;
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
