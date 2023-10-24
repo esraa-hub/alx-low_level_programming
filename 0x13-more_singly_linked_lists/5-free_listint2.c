5b39524ee59f12041ee2cdc678250922c9ae3f4c
esraa-hub <engesraa3@gmail.com>
2023-10-24 02:25:03 -0700

    F

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2-thefun.
+ *
+ * @head:the arg.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *dlt, *trans;
+
+	if (!head)
+		return;
+	trans = *head;
+	while (trans)
+	{
+		trans = dlt;
+		trans = trans->next;
+		free(dlt);
+	}
+	*head = NULL;
+}
