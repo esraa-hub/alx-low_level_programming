471c27c00a73571ca2dc4d79fa450c728ae52c32
esraa-hub <engesraa3@gmail.com>
2023-10-24 08:19:48 -0700

    G

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,14 +16,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->next = NULL;
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	temp = *head;
+	*temp = *head;
 	for (unsigned int i = 0; i < idx - 1; i++)
 	{
 		if (temp == NULL)
