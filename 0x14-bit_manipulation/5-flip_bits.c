bdf047dc9958ef993e167a200e527555ac0b4e50
esraa-hub <engesraa3@gmail.com>
2023-10-26 14:41:59 -0700

    D

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,11 +7,14 @@
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xorval = n^ m;
+	unsigned long int xorval = n ^ m;
 	unsigned int count = 0;
+
 	while (xorval)
+	{
 		if (xorval & 1ul)
 			Count++;
-	xorval - xorval >> 1;
+		xorval = xorval >> 1;
+	}
 	return (count);
 }
