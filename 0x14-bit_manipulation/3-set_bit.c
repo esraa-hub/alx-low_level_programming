89ecab20d30e4c38afaf325cf2696ba9bf4a8a92
esraa-hub <engesraa3@gmail.com>
2023-10-26 14:50:58 -0700

    D

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,13 @@
-Finclude
-"main.h"
-• set bit - sets the bit at the index
-• On: the number to index
-• Bindex: the bit to set
-* Return: 1 if successful, -1 on error int
-int set bit(unsigned long int *n, unsigned int index)
-if (index >= sizeof (n) * 8)
-return (-1);
-return KUl(*n 1- 1L «< index));
+#include "main.h"
+/**
+ * set_bit-sets the bit at the index
+ * @n: the number to index
+ * @index: the bit to se
+ * Return: 1 if successful, -1 on error int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (!!(*n |= 1L << index));
+}
