330043fb7e6b1ae19310b3acff775040b3a155d3
esraa-hub <engesraa3@gmail.com>
2023-12-07 10:27:33 -0800

    T

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint-djkvjhkhfdg
+ * @head:vdkjvnkdvndkfkdjkjg
+ * @n:djcsdjsdljdk
+ * Return:sdlknvdfkdfnl
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	distint_t *new = malloc (sizeof (distint_t));
+
+	if (!head || !new)
+		return (new ? free(new), NULL : NULL);
+	new->n = n;
+	new-›prev = NULL;
+	if (!*head)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
+	return (new);
+}
